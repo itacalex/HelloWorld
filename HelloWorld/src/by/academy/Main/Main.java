03acc79ccd706b92c40326abd0bf71d3815fae28
itacalex <32592693+itacalex@users.noreply.github.com>
2017-10-07 13:52:57 +0300

    Add files via upload
    Hello, world!

---
+++ HelloWorld/src/by/academy/Main/Main.java
@@ -0,0 +1,11 @@
+package by.academy.Main;
+
+public class Main {
+	
+	public static void log(String str){
+		System.out.println(str);
+	}
+	public static void main(String[] args) {
+		log("Hello world!");
+	}
+}
